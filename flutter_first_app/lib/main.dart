114fe959904e1e28f9ed92a639e3b9b3b18a5289
Sasha <pilevichsasha@gmail.com>
2023-05-12 19:41:57 +0300

    feat:add stateful widget

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -4,25 +4,42 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
   void answerQuestion() {
-    print('Answer chosen!');
+    setState(() {
+        questionIndex = questionIndex + 1;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What\'s your name?',
+      'How old are you?',
+      'What do you do?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz'),
         ),
         body: Column(
-          children:  [
-            Text('Please, answer questions'),
+          children: [
+            Text(questions[questionIndex]),
             ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
             ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2')),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
+            ElevatedButton(
+                onPressed: () => print('Answer 3 chosen'),
+                child: Text('Answer 3')),
           ],
         ),
       ),
