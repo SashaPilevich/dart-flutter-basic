66dab646c4dcd55554ca8d890bdb2adf413b0593
Sasha <pilevichsasha@gmail.com>
2023-05-12 18:38:54 +0300

    feat:connect function with button

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp()); 
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('First App'),),
-        body: Text('This is my default text'),
+        appBar: AppBar(
+          title: Text('Quiz'),
+        ),
+        body: Column(
+          children:  [
+            Text('Please, answer questions'),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2')),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
+          ],
+        ),
       ),
     );
   }
