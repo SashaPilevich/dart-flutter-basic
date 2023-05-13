eb93f343a5d462c340cc1ceea08a5101960db96a
Sasha <pilevichsasha@gmail.com>
2023-05-13 17:38:19 +0300

    feat:use map for questions and answers in quiz

--- flutter_first_app/lib/answer.dart
+++ flutter_first_app/lib/answer.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectedHandler;
-  Answer(this.selectedHandler);
+  final String variantOfAnswer;
+  Answer(this.selectedHandler, this.variantOfAnswer);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: ()=>selectedHandler(),
-        child: Text('Answer 1'),
+        onPressed: () => selectedHandler(),
+        child: Text(variantOfAnswer),
         style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all(Colors.blue)),
       ),
--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -24,10 +24,24 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> questions = [
-      'What\'s your name?',
-      'How old are you?',
-      'What do you do?'
+    var questions = [
+      {
+        'questionText': 'What\'s your favorite country?',
+        'answers': ['Spain', 'Portugal', 'German', 'Poland']
+      },
+      {
+        'questionText': 'What\'s your favorite coffee?',
+        'answers': ['Capuchino', 'Latte', 'Americano', 'Ruf']
+      },
+      {
+        'questionText': 'What\'s your favorite basketball team?',
+        'answers': [
+          'Boston Celtics',
+          'Brooklyn Nets',
+          'Chicago Bulls',
+          'Milwaukee Bucks'
+        ]
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -36,10 +50,11 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            Question(questions[_questionIndex]['questionText'] as String),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
